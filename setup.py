b35ded988e8e6a310535ae3a94909572db27adcd
zyegfryed <zyegfryed@gmail.com>
2012-02-24 10:41:46 +0100

    Added AUTHOR, LICENSE and detailed setup.

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
 #-*- coding: utf-8 -*-
+#!/usr/bin/env python
+"""
+Nestor
+======
+
+:copyright: (c) 2012 by the Hyperweek Team, see AUTHORS for more details.
+:lic
+"""
 from setuptools import setup, find_packages
 
 install_requires = [
@@ -10,13 +18,22 @@ install_requires = [
 setup(
     name='nestor',
     version='0.1',
-    description='Deployment app for Hyperweek',
     author=u'Sébastien Fievet',
     author_email='sebastien@hyperweek.com',
     url='https://github.com/hyperweek/nestor',
+    description='Deployment app for Hyperweek',
     packages=find_packages(),
+    zip_safe=False,
     install_requires=install_requires,
     dependency_links=[
-        'https://github.com/hyperweek/dploi-server/tarball/master#egg=dploi-server',
-    ]
+        'https://github.com/hyperweek/dploi-server/tarball/develop#egg=dploi-server',
+    ],
+    license='BSD',
+    classifiers=[
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development'
+    ],
 )
