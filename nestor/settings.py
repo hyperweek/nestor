a7d7a2fc972d3fa5122b7f14f4e9f9250cd1a72a
zyegfryed <zyegfryed@gmail.com>
2012-03-22 11:32:17 +0100

    Added lo-level API configuration for reversion.

--- nestor/settings.py
+++ nestor/settings.py
@@ -101,6 +101,8 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.transaction.TransactionMiddleware',
+    'reversion.middleware.RevisionMiddleware',
 )
 
 ROOT_URLCONF = 'nestor.urls'
