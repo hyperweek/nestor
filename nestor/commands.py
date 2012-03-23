37fd122f8191750728e8e66803f67eab9fc6898c
zyegfryed <zyegfryed@gmail.com>
2012-03-23 17:53:18 +0100

    Fixed subdomain setting for app

--- nestor/commands.py
+++ nestor/commands.py
@@ -69,10 +69,11 @@ def setup_dns(deployment, **kwargs):
     app = deployment.application
 
     base_domain = host.realm.base_domain
+    app_domain = '%s.%s' % (app.name, base_domain)
 
     dns = DNSimple(settings.DNSIMPLE_USER, settings.DNSIMPLE_PASSWORD)
     domain = dns.domains[base_domain]
-    domain.add_record(app.name, 'ALIAS', host.hostname)
+    domain.add_record(app_domain, 'ALIAS', host.hostname)
 
 
 @queue_command
