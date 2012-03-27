1bba89eff5dbd1843e866b9f5d36aeb9551ebcac
zyegfryed <zyegfryed@gmail.com>
2012-03-27 16:07:24 +0200

    Double quoted string are interpreted by puppet. Paswword has $ in it...

--- nestor/templates/app_template/app.pp
+++ nestor/templates/app_template/app.pp
@@ -2,9 +2,9 @@ include saas
 saas::instance{ "{{ app_name }}":
   domain => "{{ app_domain }}",
   user   => {
-    'username'   => "{{ app_user.username }}",
-    'email'      => "{{ app_user.email }}",
-    'password'   => "{{ app_user.password }}",
+    'username'   => '{{ app_user.username }}',
+    'email'      => '{{ app_user.email }}',
+    'password'   => '{{ app_user.password }}',
     'first_name' => "{{ app_user.first_name }}",
     'last_name'  => "{{ app_user.last_name }}",
   },
