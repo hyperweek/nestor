19aa959dd4042dfbd4d9d399895b34155526a822
zyegfryed <zyegfryed@gmail.com>
2012-03-27 09:39:23 +0200

    Fixed puppet app template to enclose data from user with double quotes, so name with single quote are fine.

--- nestor/templates/app_template/app.pp
+++ nestor/templates/app_template/app.pp
@@ -2,11 +2,11 @@ include saas
 saas::instance{ "{{ app_name }}":
   domain => "{{ app_domain }}",
   user   => {
-    'username'   => '{{ app_user.username }}',
-    'email'      => '{{ app_user.email }}',
-    'password'   => '{{ app_user.password }}',
-    'first_name' => '{{ app_user.first_name }}',
-    'last_name'  => '{{ app_user.last_name }}',
+    'username'   => "{{ app_user.username }}",
+    'email'      => "{{ app_user.email }}",
+    'password'   => "{{ app_user.password }}",
+    'first_name' => "{{ app_user.first_name }}",
+    'last_name'  => "{{ app_user.last_name }}",
   },
   ensure => {% if enabled %}'present'{% else %}'absent'{% endif %},
 }
